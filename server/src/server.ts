9be0fd9314893bd3d944bf474b4ba01140c409c4
Renan Rodrigues <renan_ime_2012@hotmail.com>
2023-02-14 19:31:23 -0300

    setup cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,13 @@
 import Fastify from 'fastify'
+import cors from '@fastify/cors'
 import { PrismaClient } from '@prisma/client'
 
 const app = Fastify()
 const prisma = new PrismaClient()
+
+// cors - cross-origin resource sharing - é um mecanismo de segurança que diz quais aplicações vão poder acessar os dados
+// se não configurar quais a aplicação frontend não vai conseguir acessar a aplicação backend
+app.register(cors)
 
 app.get('/', async ()=>{
     const habits = await prisma.habit.findMany({
